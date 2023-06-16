f4aab07e50575eabb21e4ec297d45845acc5b948
root <root@DESKTOP-9VSP7CI.localdomain>
2023-06-16 19:35:10 +0000

    2

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,10 @@
 #include <stdio.h>
+
+/**
+ * Programming is like building a multilingual puzzle
+ *
+ * Return: 0 on success
+ */
 int main(void)
 {puts("\"Programming is like building a multilingual puzzle");
 return (0);
