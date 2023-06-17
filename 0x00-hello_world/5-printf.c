80c3c4840f1f0a461b9bd86ac5ee42856c6a8646
root <root@DESKTOP-9VSP7CI.localdomain>
2023-06-17 01:07:06 +0000

    prinf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 /**
- * main - Write a C program that prints exactly with proper grammar, but the outcome is a piece of art,, followed by a new
+ * main - C program that prints exactly with proper grammar,
+ * but the outcome is a piece of art,
  *
- * Return: 0 on success
+ * Return: 0 on (success)
  */
 int main(void)
-{printf("with proper grammar, but the outcome is a piece of art,\n");
+{printf("with proper grammar, but the outcome is a piece of art");
 return (0);
 }
