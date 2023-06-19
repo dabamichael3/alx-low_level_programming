83f598cfe57570593867feb7ec6ab194c7d7f5df
root <root@DESKTOP-9VSP7CI.localdomain>
2023-06-19 22:58:59 +0000

    My first commit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -2,10 +2,9 @@
 #include <time.h>
 #include <stdio.h>
 /**
-<<<<<<< HEAD
- * main - A programthat prints the size of various data types
- *Description : 'positive'
- *return 0
+ * main - Entry point for the program 
+ *integer, calculates the last digit, and prints it.
+ * Return: 0 on success
  */
 int main(void)
 {int n;
@@ -15,38 +14,15 @@ n = rand() - RAND_MAX / 2;
 lastdigit = n % 10;
 if (lastdigit  > 5)
 {
-printf("last degit %d is %d is greater than 5\n", n, lastdigit);
+printf("last degit %d is %d is greater than 5", n, lastdigit);
 }
 else if (lastdigit == 0)
 {
-printf("last degit %d is %d is 0\n", n, lastdigit);
+printf("last degit %d is %d is 0", n, lastdigit);
 }
 else
 {
-printf("last degit %d is %d and less than 6 and not 0\n", n, lastdigit);
-=======
- * main - function is the entry point of the program.
- *description:'It calculates and displays the last digit of a given number'
- *Return: 0
- */
-int main(void)
-{int n;
-int ld;
-srand(time(0));
-n = rand() - RAND_MAX / 2;
-ld = n % 10;
-if (ld  > 5)
-{
-printf("last degit of %d is %d and  is greater than 5\n", n, ld);
-}
-else if (ld == 0)
-{
-printf("last degit of %d is %d and  is 0\n", n, ld);
-}
-else
-{
-printf("last degit of %d is %d and less than 6 and not 0\n", n, ld);
->>>>>>> 827ef70e4fb94aeea259b2baeb68823de50016e6
+printf("last degit %d is %d and less than 6 and not 0", n, lastdigit);
 }
 return (0);
 }
