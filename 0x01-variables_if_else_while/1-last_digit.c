f11454ffff1d89ea556c1d8216e17b3b84e8872d
root <root@DESKTOP-9VSP7CI.localdomain>
2023-06-19 22:40:57 +0000

    My first commit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,30 +1,28 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-/**                                                                                                                    
- *main - The main function is
- * the entry point of the program.                                                          
- * It executes the necessary operations to determine                                                                   
- * if a number is positive or negative.                                                                                
- * Return: 0                                                                                                           
+/**
+ * main - A programthat prints the size of various data types
+ *Description : 'positive'
+ *return 0
  */
 int main(void)
 {int n;
-  int lastdigit;
-  srand (time(0));
-  n = rand () - RAND_MAX / 2;
-  lastdigit = n % 10;
-  if (lastdigit  > 5)
-    {
-      printf("last degit %d is %d is greater than 5", n, lastdigit);
-    }
-  else if (lastdigit == 0)
-    {
-      printf("last degit %d is %d is 0", n, lastdigit);
-    }
-  else
-    {
-      printf("last degit %d is %d and less than 6 and not 0", n, lastdigit);
-    }
-  return (0);
+int lastdigit;
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+lastdigit = n % 10;
+if (lastdigit  > 5)
+{
+printf("last degit %d is %d is greater than 5\n", n, lastdigit);
+}
+else if (lastdigit == 0)
+{
+printf("last degit %d is %d is 0\n", n, lastdigit);
+}
+else
+{
+printf("last degit %d is %d and less than 6 and not 0\n", n, lastdigit);
+}
+return (0);
 }
