753a3c31b1b89136ab460d21903e76990bd61843
root <root@DESKTOP-9VSP7CI.localdomain>
2023-06-26 14:10:55 +0000

    My first commit

--- 0x05-pointers_arrays_strings/0-main.c
+++ 0x05-pointers_arrays_strings/0-main.c
@@ -1,9 +1,10 @@
 #include "main.h"
 #include <stdio.h>
 /**
-*main-check the code 
-*Return:Always 0.
-*/
+ * main - check the code.
+ *
+ * Return: Always 0.
+ */
 int main(void)
 {int n;
 n = 402;
